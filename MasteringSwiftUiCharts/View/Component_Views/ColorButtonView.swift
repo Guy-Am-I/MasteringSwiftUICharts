809f1f03701147155ad3352e8ed86fd257417057
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-16 20:07:05 -0700

    Added color extension from course resources and empty ColorButtonView

---
+++ MasteringSwiftUiCharts/View/Component_Views/ColorButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorButtonView.swift
+//  MasteringSwiftUiCharts
+//
+//  Created by Guy Steinberg on 2024-04-16.
+//
+
+import SwiftUI
+
+struct ColorButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorButtonView()
+}
