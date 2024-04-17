b7a1c07964d30bcda34212332b9a2d214a178f9d
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-16 18:23:59 -0700

    Initial Commit

---
+++ MasteringSwiftUiCharts/MasteringSwiftUiChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringSwiftUiChartsApp.swift
+//  MasteringSwiftUiCharts
+//
+//  Created by Guy Steinberg on 2024-04-16.
+//
+
+import SwiftUI
+
+@main
+struct MasteringSwiftUiChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
